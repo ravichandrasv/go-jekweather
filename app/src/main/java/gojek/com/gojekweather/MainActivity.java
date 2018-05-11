5ff26fa05852365f13a876075f7f1c8011dfa5ca
Ravichandra S V <ravichandra.sv@accionlabs.com>
2018-05-11 17:31:00 +0530

    initial push

---
+++ app/src/main/java/gojek/com/gojekweather/MainActivity.java
@@ -0,0 +1,13 @@
+package gojek.com.gojekweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
